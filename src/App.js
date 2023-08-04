6c4f71ccded3f2ea8e38331c685eca77028bc57c
Tamizhchemmal <tamizhchemmal@gmail.com>
2023-08-04 12:24:39 +0530

    fun

--- src/App.js
+++ src/App.js
@@ -11,20 +11,9 @@ import RefProfile from "./Components/RefProfile";
 import React, { useState } from "react";
 
 import "./Css/login.css";
-import boss from "./Assets/Images/boss.png";
-import { useNavigate } from "react-router-dom";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import { teal } from "@mui/material/colors";
-import Modal from "react-bootstrap/Modal";
 
-import { TextField, Button } from "@mui/material";
 import Noaccess from "./Components/Noaccess";
 import Adminlogin from "./Components/Adminlogin";
-
 
 function App() {
   const getrole = (role) => {
--- src/Components/RefProfile.jsx
+++ src/Components/RefProfile.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 import "../Css/Refprofile.css";
-import NavBar from './NavBar';
-import RefProfModal from './RefProfModal';
+import NavBar from "./NavBar";
+import RefProfModal from "./RefProfModal";
 
 export default function RefProfile() {
-
-  return(
+  return (
     <>
-        <div className="ref-profile">
-            <div id="navs">
-                <NavBar  />
-            </div>
-            <div className="ref-modal">
-                <RefProfModal />
-            </div>
+      <div className="ref-profile">
+        <div id="navs">
+          <NavBar />
         </div>
+        <div className="ref-modal">
+          <RefProfModal />
+        </div>
+      </div>
     </>
   );
 }
